8716f48abc41ebc6064e0911cf1475d9995ba5da
DESKTOP-3IMUCLA\Adam <adam517rhouni@gmail.com>
2017-03-01 21:16:48 +0100

    stupid icons

--- src/components/ChatDisplay.js
+++ src/components/ChatDisplay.js
@@ -40,9 +40,11 @@ class ChatDisplay extends React.Component {
   changeTitle() {
     if (document.hasFocus()){
       document.title=('Summerdeaf Client');
+      document.querySelector("link[rel='shortcut icon']").href = "http://i.imgur.com/vK1uLpy.png";
     }
     if (!document.hasFocus()){
       document.title=('New Messages');
+      document.querySelector("link[rel='shortcut icon']").href = "http://i.imgur.com/N9Hf1nq.png";
     }
   }
   componentDidUpdate (prevProps) {
@@ -55,7 +57,10 @@ class ChatDisplay extends React.Component {
     this.timer = setInterval(function () {
       this.forceUpdate();
     }.bind(this), 1000);
-    window.onfocus = function() {document.title=('Summerdeaf Client')};
+    window.onfocus = function() {
+      document.title=('Summerdeaf Client');
+      document.querySelector("link[rel='shortcut icon']").href = "http://i.imgur.com/vK1uLpy.png";
+    };
     socket.off('new message');
     socket.on('new message', function (newMessages) {
       this.props.addMessages(newMessages);
